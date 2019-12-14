c5d7fd8d87c24d7a0757f13b56f318c8aa8aa806
k2Raven <leha.21.05.1996@gmail.com>
2019-12-14 15:45:43 +0600

    Создал Добавления фотографии просмотра

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from webapp.views import IndexView, PhotoView
+from webapp.views import IndexView, PhotoView, PhotoCreateView
 
 app_name = 'webapp'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('photo/<int:pk>/', PhotoView.as_view(), name='detail_photo')
+    path('photo/<int:pk>/', PhotoView.as_view(), name='detail_photo'),
+    path('photo/create/', PhotoCreateView.as_view(), name='photo_create'),
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,5 +1,10 @@
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from webapp.models import Photo
+from webapp.forms import PhotoForm
+
+from django.urls import reverse_lazy
 
 
 class IndexView(ListView):
@@ -12,3 +17,21 @@ class PhotoView(DetailView):
     template_name = 'detail.html'
     model = Photo
     context_object_name = 'photo'
+
+
+class PhotoCreateView(LoginRequiredMixin, CreateView):
+    template_name = 'create.html'
+    model = Photo
+    form_class = PhotoForm
+    success_url = reverse_lazy('webapp:index')
+
+    def form_valid(self, form):
+        self.object = self.model.objects.create(author=self.request.user, **form.cleaned_data)
+        return redirect(self.get_success_url())
+
+class PhotoUpdateView(UpdateView):
+    template_name = 'create.html'
+    model = Photo
+    form_class = PhotoForm
+    success_url = reverse_lazy('webapp:index')
+
