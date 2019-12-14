47d44e64640d6e1d1436af20b2ea49ae8d742702
k2Raven <leha.21.05.1996@gmail.com>
2019-12-14 17:53:48 +0600

    "лайк"  добавления и уменьшение

--- source/api_v1/views.py
+++ source/api_v1/views.py
@@ -1,4 +1,6 @@
+from rest_framework.decorators import action
 from rest_framework import viewsets
+from rest_framework.response import Response
 from webapp.models import Comments, Photo
 from api_v1.serializers import CommentsSerializer, PhotoSerializer
 
@@ -11,4 +13,18 @@ class CommentsViewSet(viewsets.ModelViewSet):
 class PhotoViewSet(viewsets.ModelViewSet):
     queryset = Photo.objects.all()
     serializer_class = PhotoSerializer
+
+    @action(methods=['post'], detail=True)
+    def like_up(self, request, pk=None):
+        photo = self.get_object()
+        photo.likes += 1
+        photo.save()
+        return Response({'id': photo.pk, 'rating': photo.likes})
+
+    @action(methods=['post'], detail=True)
+    def like_down(self, request, pk=None):
+        photo = self.get_object()
+        photo.likes -= 1
+        photo.save()
+        return Response({'id': photo.pk, 'rating': photo.likes})
 
