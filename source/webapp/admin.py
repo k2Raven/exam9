bf520a3fa20b244d8d3a9e1685b28a8f19805a7e
k2Raven <leha.21.05.1996@gmail.com>
2019-12-14 13:20:52 +0600

    Этап 1 Модели доделал

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from webapp.models import Photo, Comments, Like
 
-# Register your models here.
+admin.site.register(Photo)
+
+admin.site.register(Comments)
+
+admin.site.register(Like)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -4,19 +4,30 @@ from django.contrib.auth.models import User
 
 class Photo(models.Model):
     photo = models.ImageField(null=True, blank=True, upload_to='photo', verbose_name='Фотография')
-    caption = models.CharField(verbose_name='Подпись')
+    caption = models.CharField(max_length=300, verbose_name='Подпись')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата-время создания')
     likes = models.IntegerField(default=0, verbose_name='Лайки')
-    author = models.ForeignKey(User, related_name='photo', on_delete=models.CASCADE, verbose_name='Автор фотографии')
+    author = models.ForeignKey(User, related_name='photo',
+                               on_delete=models.CASCADE, verbose_name='Автор фотографии')
+
+    def __str__(self):
+        return self.caption
 
 
 class Comments(models.Model):
-    photo = models.ForeignKey('webapp.Photo', related_name='comments', on_delete=models.CASCADE, verbose_name='Фотография')
-    text = models.TextField(max_length=400, verbose_name='Комментарий')
-    author = models.ForeignKey(User, related_name='photo', on_delete=models.CASCADE, verbose_name='Автор комментария')
+    photo = models.ForeignKey('webapp.Photo', related_name='comments',
+                              on_delete=models.CASCADE, verbose_name='Фотография')
+    text = models.TextField(max_length=700, verbose_name='Комментарий')
+    author = models.ForeignKey(User, related_name='comments',
+                               on_delete=models.CASCADE, verbose_name='Автор комментария')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата-время создания')
 
+    def __str__(self):
+        return self.text
 
 
 class Like(models.Model):
-    pass
+    photo = models.ForeignKey('webapp.Photo', related_name='like',
+                              on_delete=models.CASCADE, verbose_name='Фотография')
+    author = models.ForeignKey(User, related_name='like',
+                               on_delete=models.CASCADE, verbose_name='Автор лайка')
